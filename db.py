cea62bb1f1b39735a41a155d370d891e6d62c50c
ilR <qazcom203@gmail.com>
2025-05-11 20:30:52 +0300

    test

---
+++ db.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, func
+from sqlalchemy.orm import declarative_base
+
+# 1. Создаём базовый класс
+Base = declarative_base()
+
+# 2. Описываем таблицу как Python-класс
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    tg_id = Column(Integer, unique=True, nullable=False)
+    first_name = Column(String)
+    registered_at = Column(DateTime, server_default=func.now())
+
+# 3. Создаём движок SQLite (файл создастся в корне)
+engine = create_engine('sqlite:///db.db')
+
+# 4. Создаём таблицы в БД
+Base.metadata.create_all(engine)
+
+print("База данных и таблица созданы!")
--- main.py
+++ main.py
@@ -13,10 +13,16 @@ dp = Dispatcher()
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    if message.from_user.id != 5335241383:
+        await message.answer("Hello!" + str(message.from_user.first_name))
+    else:
+        await message.answer("Hello Oleg")
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
